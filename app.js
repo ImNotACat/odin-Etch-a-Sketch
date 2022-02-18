27ecdea378953cedea15f2ea39a62512365c955b
Katya <k.vezzadini@gmail.com>
2022-02-18 20:45:39 +0000

    Custom size grid

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
+const STANDARD_SIZE = 20;
+
 // get element from page
 let body = document.getElementById("sketch");
 console.log("sketch");
 
-// create the rows - add 16x for loop
-for (let i =0; i < 30; i++) {
-    var row = document.createElement("div");
-    console.log(row);
-    row.className = "row";
-    row.id = "row" + i;
+function createGrid() {
+    let size = window.prompt("What is the size of your grid? 1-100")
     
-    // create the squares - add 16x for loop
-    for (let j = 0; j < 30; j++){
-        var square = document.createElement("div");
-        square.className = "square";
-        square.id = "square" + j;
-        row.append(square);
+// create the rows - add 16x for loop
+    for (let i =0; i < size; i++) {
+        var row = document.createElement("div");
+        console.log(row);
+        row.className = "row";
+        row.id = "row";
+        
+        // create the squares - add 16x for loop
+        for (let j = 0; j < size; j++){
+            var square = document.createElement("div");
+            square.className = "square";
+            square.id = "square";
+            row.append(square);
+        }
+
+        // append divs to DOM
+        body.append(row);
+
     }
-
-    // append divs to DOM
-    body.append(row);
 }
-
-let squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", func, false);
-})
-
 
 function func() {
     this.setAttribute("style", "background-color: #7689de")
 }
+
+window.onload = createGrid();
+
+let squares = document.querySelectorAll(".square");
+console.log(squares);
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", func, false);
+})
